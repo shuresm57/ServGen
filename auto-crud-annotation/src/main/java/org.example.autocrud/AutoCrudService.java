ae1b10877b577c36436a1dee2e4254587035d66e
shuresm57 <vsorensen31@gmail.com>
2025-12-15 21:32:03 +0100

    created annotation class

--- auto-crud-annotation/src/main/java/org.example.autocrud/AutoCrudService.java
+++ auto-crud-annotation/src/main/java/org.example.autocrud/AutoCrudService.java
@@ -1,4 +1,15 @@
 package org.example.autocrud;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
 public @interface AutoCrudService {
+
+    Class<?> entity();
+    Class<?> repository();
+
 }
