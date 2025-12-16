da171e7913b6031f2bde0ac1de2a5f0c1f8d9a0d
shuresm57 <vsorensen31@gmail.com>
2025-12-16 11:30:40 +0100

    fixed test dependencies

--- demo-app/src/main/java/com/example/demoapp/CarService.java
+++ demo-app/src/main/java/com/example/demoapp/CarService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 @AutoCrudService(entity = Car.class, repository = CarRepository.class)
 public class CarService extends BaseCarService {
-    
     public CarService(CarRepository repository) {
         super(repository);
     }
