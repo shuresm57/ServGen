cedcf241735f0aba99e9ca8120f7e4c7478d7304
shuresm57 <vsorensen31@gmail.com>
2025-12-17 11:39:33 +0100

    h2 dependencies

--- demo-app/src/main/java/com/example/demoapp/model/Car.java
+++ demo-app/src/main/java/com/example/demoapp/model/Car.java
@@ -1,15 +1,24 @@
 package com.example.demoapp.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "cars")
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class Car {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    
+    @Column(name = "color")
     String color;
+    
+    @Column(name = "reg_no")
     int regNo;
 
 }
